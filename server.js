0f43475a6b3b54838370c0dbc5e82e9f857b571b
Glitch (webware-pong-final-project) <none>
2020-10-14 15:07:24 +0000

    🙆👂 Checkpoint ./server.js:21928091/4285

--- server.js
+++ server.js
@@ -5,30 +5,15 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const WebSocket = require("ws");
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const wss = new WebSocket.Server({port: 8082});
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+wss.on("connection", ws =>{
+  console.log("new client connected!");
+  
+  
+  ws.on("close", ()=>{
+    consol
+  })
 });
