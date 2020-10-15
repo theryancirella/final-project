9856d860a364b987648ac9d1b7355d5bd74adfb5
Glitch (webware-pong-final-project) <none>
2020-10-15 14:03:36 -0400

    final Project

--- public/script.js
+++ public/script.js
@@ -1,24 +1,119 @@
 
 console.log("hello world :o");
 
+
+
 var animate = window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function(callback) { window.setTimeout(callback, 1000/60) };
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+function(callback) { window.setTimeout(callback, 1000/60) };
 
 var canvas = document.createElement('canvas');
 var width = 400;
 var height = 600;
+
+var playerX = 0;
+var computerX = 0;
+
+var p2Ballx = 0;
+var p2Bally = 0;
+
+var currPointHome = 0;
+var currPointAway = 0;
+
+
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
+var playerNumber = 0;
+
+var leftDiv = document.getElementById('left') // For putting the game above
+
+
+
+//***************************player select socket*******************
+const firstws = new WebSocket("ws://192.168.1.114:6969");
+
+firstws.addEventListener("open", () => {
+  //console.log("connected");
+
+  firstws.addEventListener("message", something=>{
+
+    //console.log(something.data);
+    let obj1 = JSON.parse(something.data);
+
+    let x = obj1['PlayerNum'];
+
+    //console.log("x:" + x);
+    
+    if(x == 1){
+      playerNumber = 1;
+    } else if(x == 2){
+      playerNumber = 2;
+    }
+    //console.log("Player Number: " + playerNumber);
+
+
+    firstws.close();
+  }); 
+  
+
+
+});
+
+function timeout() {
+  setTimeout(function () {
+  }, 5000);
+}
+
+timeout();
+
+//*******************Game socket************************
+const ws = new WebSocket("ws://192.168.1.114:8082");
+
+
+ws.addEventListener("message", e=>{
+
+  //console.log(e.data);
+  let obj1 = JSON.parse(e.data);
+
+  let x = obj1['ComputerX'];
+  let p2bx = obj1['BallX'];
+  let p2by = obj1['BallY'];
+    //console.log(x);
+    computerX = x;
+    p2Ballx = 400 - p2bx;
+    p2Bally = 600 - p2by;
+  }); 
+
+
+
+ws.addEventListener("open", () => {
+  //console.log("connected");
+
+  let firstmsg = {
+    PlayerX: player.paddle.x
+  }
+  
+  ws.send(JSON.stringify(firstmsg));
+
+  
+
+  
+
+});
+
+
+
 
 window.onload = function() {
-  document.body.appendChild(canvas);
+  leftDiv.appendChild(canvas);
   animate(step);
+  getItems(playerNumber);
 };
 
 var step = function() {
+
   update();
   render();
   animate(step);
@@ -43,13 +138,13 @@ function Paddle(x, y, width, height) {
 }
 
 Paddle.prototype.render = function() {
-  context.fillStyle = "#0000FF";
+  context.fillStyle = "#0ff";
   context.fillRect(this.x, this.y, this.width, this.height);
 };
 
 
 function Player() {
-   this.paddle = new Paddle(175, 580, 50, 10);
+ this.paddle = new Paddle(175, 580, 50, 10);
 }
 
 function Computer() {
@@ -76,7 +171,7 @@ function Ball(x, y) {
 Ball.prototype.render = function() {
   context.beginPath();
   context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
-  context.fillStyle = "#000000";
+  context.fillStyle = "#FFF";
   context.fill();
 };
 
@@ -86,7 +181,7 @@ var computer = new Computer();
 var ball = new Ball(200, 300);
 
 var render = function() {
-  context.fillStyle = "#FF00FF";
+  context.fillStyle = "#000";
   context.fillRect(0, 0, width, height);
   player.render();
   computer.render();
@@ -106,13 +201,29 @@ var update = function() {
   ball.update(player.paddle, computer.paddle);
 };
 
-Ball.prototype.update = function(paddle1, paddle2) {
-  this.x += this.x_speed;
-  this.y += this.y_speed;
-  var top_x = this.x - 5;
-  var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
-  var bottom_y = this.y + 5;
+Ball.prototype.update = function(paddle1, paddle2, bx, by) {
+
+  if(playerNumber == 2){
+    if(by <= 0){
+      addScore(2, 1);
+    }
+
+    this.x = bx;
+    this.y = by;
+   
+
+
+
+} else {
+ this.x += this.x_speed;
+ this.y += this.y_speed;
+ var top_x = this.x - 5;
+ var top_y = this.y - 5;
+ var bottom_x = this.x + 5;
+ var bottom_y = this.y + 5;
+
+
+
 
   if(this.x - 5 < 0) { // hitting the left wall
     this.x = 5;
@@ -122,11 +233,24 @@ Ball.prototype.update = function(paddle1, paddle2) {
     this.x_speed = -this.x_speed;
   }
 
-  if(this.y < 0 || this.y > 600) { // a point was scored
+  if(this.y < 0) { // a point was scored
+    addScore(1, 1);
+    currPointAway = 0;
+    currPointHome = 0;
     this.x_speed = 0;
     this.y_speed = 3;
     this.x = 200;
     this.y = 300;
+    
+    
+  }
+
+  if(this.y > 600){
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+    
   }
 
   if(top_y > 300) {
@@ -135,6 +259,7 @@ Ball.prototype.update = function(paddle1, paddle2) {
       this.y_speed = -3;
       this.x_speed += (paddle1.x_speed / 2);
       this.y += this.y_speed;
+      currPointHome++;
     }
   } else {
     if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
@@ -144,6 +269,8 @@ Ball.prototype.update = function(paddle1, paddle2) {
       this.y += this.y_speed;
     }
   }
+}
+
 };
 
 var keysDown = {};
@@ -192,22 +319,99 @@ Paddle.prototype.move = function(x, y) {
 
 var update = function() {
   player.update();
-  computer.update(ball);
-  ball.update(player.paddle, computer.paddle);
+  ball.update(player.paddle, computer.paddle, p2Ballx, p2Bally);
+  computer.update(computerX);
+
+  let msg = {
+    PlayerNum: playerNumber,
+    PlayerX: player.paddle.x,
+    BallX: ball.x,
+    BallY: ball.y
+  }
+
+  ws.send(JSON.stringify(msg));
+  
+
+
 };
 
-Computer.prototype.update = function(ball) {
-  var x_pos = ball.x;
-  var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
-  if(diff < 0 && diff < -4) { // max speed left
-    diff = -5;
-  } else if(diff > 0 && diff > 4) { // max speed right
-    diff = 5;
-  }
-  this.paddle.move(diff, 0);
-  if(this.paddle.x < 0) {
-    this.paddle.x = 0;
-  } else if (this.paddle.x + this.paddle.width > 400) {
-    this.paddle.x = 400 - this.paddle.width;
-  }
+Computer.prototype.update = function(newx) {
+  computerX = newx;
+  this.paddle.x = newx;
 };
+
+
+//*****************database stuff*********
+
+let user = "testuser"; //this will be the user's username. Replace dynamically with login
+
+
+const scoresList = document.getElementById("scores");
+
+
+
+//Score MUST be taken in as string
+function addScore(playerNum, theScore){
+  fetch("/add", {
+    method: "POST",
+    body: JSON.stringify({ score: theScore, PlayerNum: playerNum }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(res => clearList())
+  .then(res2 => getItems(playerNum))
+}
+
+//Add score to list helper function:
+function appendNewScore(score, id) {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = score;
+  scoresList.appendChild(newListItem);
+  newListItem.onclick = function() {
+    deleteItem(id);
+    newListItem.remove();
+  }
+}
+
+//Delete Function:
+function deleteItem(id) {
+  fetch("/delete", {
+    method: "POST",
+    body: JSON.stringify({ id }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => response.json())
+  .then(json => {});
+}
+
+//Get scores from server
+function getItems(playerNum) {
+  fetch("/items", {
+    method: "POST",
+    body: JSON.stringify({ PlayerNum: playerNum }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json()) // parse the JSON from the server
+    .then(scores => {
+      // remove the loading text
+
+      console.log("Items in database:");
+      console.log(scores);
+      for (var i = 0; i < scores.length; i++) {
+        appendNewScore(scores[i].score, scores[i]._id);
+      }
+    });
+  }
+
+//Clear the ul list
+function clearList(){
+  scoresList.innerHTML = "";
+}
+
+//RYANS CODE ENDS
